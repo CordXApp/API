3d40562e144f83d1a94cfdf9d0568510c0b385b1
TyDoesDev <tydoesdev@gmail.com>
2023-08-07 02:31:46 -0600

    feat(add): status comp filters

---
+++ src/router/v3/status/compfilter.js
@@ -0,0 +1,172 @@
+const config = require('@configs/main');
+
+module.exports = async (fastify, opts) => {
+    fastify.get('/comp/filter/:secret', async (request, reply) => {
+
+        reply.header('Content-Type', 'application/json');
+
+        switch(request.query.method) {
+
+            case "dns":
+            
+                if (!request.params.secret) return reply.code(400).send({
+                    message: 'Please provide the required secret query',
+                    error: true,
+                    status: 400
+                })
+    
+                if (request.params.secret !== config.status) return reply.code(400).send({
+                    message: 'Invalid secret provided.',
+                    error: true,
+                    status: 400
+                })
+
+                const dnsRes = await fetch('https://api.instatus.com/v2/ckq7ppm4g124913arohzh7jn9ek/components', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${request.params.secret}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+
+                const dnsComp = await dnsRes.json();
+                const dnsCompRes = await dnsComp.filter((d) => d.name === 'DNS');
+                
+                return reply.code(200).send({
+                    results: dnsCompRes
+                });
+
+            case "sl":
+
+                if (!request.params.secret) return reply.code(400).send({
+                    message: 'Please provide the required secret query',
+                    error: true,
+                    status: 400
+                })
+    
+                if (request.params.secret !== config.status) return reply.code(400).send({
+                    message: 'Invalid secret provided.',
+                    error: true,
+                    status: 400
+                })
+
+                const slRes = await fetch('https://api.instatus.com/v2/ckq7ppm4g124913arohzh7jn9ek/components', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${request.params.secret}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+
+                const slComp = await slRes.json();
+                const slCompRes = await slComp.filter((d) => d.name === 'Short Links');
+                
+                return reply.code(200).send({
+                    results: slCompRes
+                });
+
+            case "tp":
+
+                if (!request.params.secret) return reply.code(400).send({
+                    message: 'Please provide the required secret query',
+                    error: true,
+                    status: 400
+                })
+    
+                if (request.params.secret !== config.status) return reply.code(400).send({
+                    message: 'Invalid secret provided.',
+                    error: true,
+                    status: 400
+                })
+
+                const tpRes = await fetch('https://api.instatus.com/v2/ckq7ppm4g124913arohzh7jn9ek/components', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${request.params.secret}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+
+                const tpComp = await tpRes.json();
+                const tpCompRes = await tpComp.filter((d) => d.name === 'Short Links');
+                
+                return reply.code(200).send({
+                    results: tpCompRes
+                });
+
+            case "us":
+
+                if (!request.params.secret) return reply.code(400).send({
+                    message: 'Please provide the required secret query',
+                    error: true,
+                    status: 400
+                })
+    
+                if (request.params.secret !== config.status) return reply.code(400).send({
+                    message: 'Invalid secret provided.',
+                    error: true,
+                    status: 400
+                })
+
+                const usRes = await fetch('https://api.instatus.com/v2/ckq7ppm4g124913arohzh7jn9ek/components', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${request.params.secret}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+
+                const usComp = await usRes.json();
+                const usCompRes = await usComp.filter((d) => d.name === 'Short Links');
+                
+                return reply.code(200).send({
+                    results: usCompRes
+                });
+
+                case "web":
+
+                if (!request.params.secret) return reply.code(400).send({
+                    message: 'Please provide the required secret query',
+                    error: true,
+                    status: 400
+                })
+    
+                if (request.params.secret !== config.status) return reply.code(400).send({
+                    message: 'Invalid secret provided.',
+                    error: true,
+                    status: 400
+                })
+
+                const webRes = await fetch('https://api.instatus.com/v2/ckq7ppm4g124913arohzh7jn9ek/components', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${request.params.secret}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+
+                const webComp = await webRes.json();
+                const webCompRes = await webComp.filter((d) => d.name === 'Short Links');
+                
+                return reply.code(200).send({
+                    results: webCompRes
+                });
+                
+                
+
+            default:
+
+                return reply.code(400).send({
+                    message: 'Invalid method query provided',
+                    required: 'One of: ["dns", "sl", "tp", "us", "web"]',
+                    error: true,
+                    status: 400
+                })
+        }
+    })
+}
--- src/router/v3/system/versions.js
+++ src/router/v3/system/versions.js
@@ -8,14 +8,14 @@ module.exports = async (fastify, opts) => {
 
         return reply.code(200).send({
             current: {
-                api: `v${await sendGithubRequest({ repo: 'CordXApp/Website', branch: 'master', path: 'package.json'})}`, 
+                api: `v${await sendGithubRequest({ repo: 'CordXApp/OldWebsite', branch: 'master', path: 'package.json'})}`, 
                 client: `v${await sendGithubRequest({ repo: 'CordXApp/Client', branch: 'master', path: 'package.json'})}`,
-                website: `v${await sendGithubRequest({ repo: 'CordXApp/Website', branch: 'master', path: 'package.json'})}`
+                website: `v${await sendGithubRequest({ repo: 'CordXApp/OldWebsite', branch: 'master', path: 'package.json'})}`
             },
             newest: {
                 api: `v${await sendGithubRequest({ repo: 'CordXApp/API', branch: 'master', path: 'package.json'})}`,
                 client: `v${await sendGithubRequest({ repo: 'CordXApp/Client', branch: 'master', path: 'package.json'})}`,
-                website: `v${await sendGithubRequest({ repo: 'CordXApp/Nextjs', branch: 'master', path: 'package.json'})}`
+                website: `v${await sendGithubRequest({ repo: 'CordXApp/Website', branch: 'master', path: 'package.json'})}`
             },
             stable: {
                 api: `v${await sendGithubRequest({ repo: 'CordXApp/API', branch: 'master', path: 'package.json'})}`, 
