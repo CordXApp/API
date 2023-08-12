404a1c117169459d750368eb8b2d2ae3975d6dbc
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2023-08-12 02:43:50 -0600

    Update summary.js

--- src/router/v3/status/summary.js
+++ src/router/v3/status/summary.js
@@ -2,21 +2,9 @@ const config = require('@configs/main');
 
 module.exports = async (fastify, opts) => {
 
-    fastify.get('/summary/:secret', async (request, reply) => {
+    fastify.get('/summary', async (request, reply) => {
 
         reply.header('Content-Type', 'application/json');
-
-        if (!request.params.secret) return reply.code(400).send({
-            message: 'Please provide the required secret query',
-            error: true,
-            status: 400
-        })
-
-        if (request.params.secret !== config.status) return reply.code(400).send({
-            message: 'Invalid secret provided.',
-            error: true,
-            status: 400
-        })
 
 
         const response = await fetch('https://cordx.instatus.com/summary.json', {
