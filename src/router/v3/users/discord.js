9b6fa982c6ff5f981bfcca1c791a2d0b159cdf28
TheRealToxicDev <toxic.dev09@gmail.com>
2023-11-21 01:59:14 -0700

    fix(bruhh): update version api and remove caching

--- src/router/v3/users/discord.js
+++ src/router/v3/users/discord.js
@@ -80,47 +80,6 @@ module.exports = async function (fastify) {
                         }
                     }
                 },
-                206: {
-                    description: 'Partial/Cached content',
-                    type: 'object',
-                    properties: {
-                        cached: {
-                            type: 'boolean',
-                            default: true,
-                            description: 'Will always be true'
-                        },
-                        expiry: {
-                            type: 'number',
-                            default: 1800,
-                            description: '30 minutes'
-                        },
-                        data: {
-                            type: 'object',
-                            properties: {
-                                id: { type: 'string' },
-                                username: { type: 'string' },
-                                global_name: { type: 'string' },
-                                avatar: {
-                                    type: 'object',
-                                    properties: {
-                                        id: { type: 'string' },
-                                        link: { type: 'string' },
-                                        is_animated: { type: 'boolean' }
-                                    }
-                                },
-                                banner: {
-                                    type: 'object',
-                                    properties: {
-                                        id: { type: 'string' },
-                                        link: { type: 'string' },
-                                        is_animated: { type: 'boolean' },
-                                        color: { type: 'string' }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                },
                 404: {
                     description: 'User not found',
                     type: 'object',
@@ -168,76 +127,45 @@ module.exports = async function (fastify) {
         handler: async (request, reply) => {
             try {
                 const { userId } = request.params
-                const cached = await request.client._cache.get(`discord_${userId}`)
-                const parsed = await JSON.parse(cached)
 
-                if (cached) {
-                    reply.code(206).send(
-                        JSON.stringify({
-                            cached: true,
-                            expiry: 1800,
-                            data: {
-                                id: parsed.id,
-                                created_at: parsed.created_at,
-                                username:
-                                    parseInt(parsed.discriminator) > 0
-                                        ? `${parsed.username}#${parsed.discriminator}`
-                                        : parsed.username,
-                                global_name: parsed.global_name,
-                                avatar: parsed.avatar,
-                                banner: parsed.banner
+                await fetch(`https://canary.discord.com/api/v10/users/${userId}`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bot ${config.token}`
+                    }
+                })
+                    .then(res => res.json())
+                    .then(user => {
+                        let avatarLink = null
+
+                        if (user.avatar) avatarLink = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`
+
+                        let bannerLink = null
+                        if (user.banner) bannerLink = `https://cdn.discordapp.com/banners/${user.id}/${user.banner}`
+
+                        const output = {
+                            id: user.id,
+                            created_at: snowflakeToDate({ snowflake: user.id }),
+                            username:
+                                parseInt(user.discriminator) > 0
+                                    ? `${user.username}#${user.discriminator}`
+                                    : user.username,
+                            global_name: user.global_name,
+                            avatar: {
+                                id: user.avatar,
+                                link: avatarLink,
+                                is_animated: user.avatar !== null && user.avatar.startsWith('a_') ? true : false
+                            },
+                            banner: {
+                                id: user.banner,
+                                link: bannerLink,
+                                is_animated: user.banner !== null && user.banner.startsWith('a_') ? true : false,
+                                color: user.banner_color
                             }
-                        })
-                    )
-                } else {
-                    await fetch(`https://canary.discord.com/api/v10/users/${userId}`, {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bot ${config.token}`
                         }
+
+                        return reply.code(200).send(JSON.stringify(output))
                     })
-                        .then(res => res.json())
-                        .then(user => {
-                            let avatarLink = null
-
-                            if (user.avatar) avatarLink = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`
-
-                            let bannerLink = null
-                            if (user.banner) bannerLink = `https://cdn.discordapp.com/banners/${user.id}/${user.banner}`
-
-                            const output = {
-                                id: user.id,
-                                created_at: snowflakeToDate({ snowflake: user.id }),
-                                username:
-                                    parseInt(user.discriminator) > 0
-                                        ? `${user.username}#${user.discriminator}`
-                                        : user.username,
-                                global_name: user.global_name,
-                                avatar: {
-                                    id: user.avatar,
-                                    link: avatarLink,
-                                    is_animated: user.avatar !== null && user.avatar.startsWith('a_') ? true : false
-                                },
-                                banner: {
-                                    id: user.banner,
-                                    link: bannerLink,
-                                    is_animated: user.banner !== null && user.banner.startsWith('a_') ? true : false,
-                                    color: user.banner_color
-                                }
-                            }
-
-                            request.client._cache
-                                .set(`discord_${userId}`, JSON.stringify(output), 'EX', 1800)
-                                .then(() => {
-                                    console.log(`User: ${userId} cached for 30 minutes`)
-                                })
-                                .catch(e => {
-                                    console.error(`Failed caching user: ${userId} | Error: ${e.stack}`)
-                                })
-
-                            return reply.code(200).send(JSON.stringify(output))
-                        })
-                }
             } catch (err) {
                 await console.error(err.stack)
 
